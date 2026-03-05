540b0294cece639f4d7dbec82635eea601aa2563
WereAM <michelle.ambunya@gmail.com>
2026-03-05 17:12:55 +0000

    Add skills-related url

--- skills/urls.py
+++ skills/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'skills'
 
 urlpatterns = [
-    path('', views.list, name='list'),
+    path('', views.skill_list, name='list'),
+    path('mine/', views.my_skills, name='my_skills'),
+    path('add/', views.add_user_skill, name='add'),
+    path('<int:pk>/', views.skill_detail, name='details'),
+    path('<int:pk>/edit/', views.edit__user_skill, name='edit'),
+    path('<int:pk>/delete/', views.delete_user_skill, name='delete'),
 ]
