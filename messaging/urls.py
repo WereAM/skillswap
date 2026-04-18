29e8498d15fb0a1981b14d97a9d513efc0a99015
Michelle Were Ambunya <82042951+WereAM@users.noreply.github.com>
2026-04-18 13:24:27 +0100

    create messaging app
    * messaging app: send messages, view conversation threads
    
    * display unread messages on navbar
    
    * display previous messages

--- messaging/urls.py
+++ messaging/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'messaging'
 
 urlpatterns = [
-    
+    # to all conversations
+    path('', views.inbox, name='inbox'),
+    #  to a specific conversation thread
+    path('<str:username>/', views.conversation, name='conversation'),
 ]
